f6698b8333fd3478b9b8e25ceb263ed66673d481
Cai Zichen <3234912331@qq.com>
2026-02-14 14:04:10 +0800

    完成 hw02 作业

--- HW/hw02/hw02.py
+++ HW/hw02/hw02.py
@@ -32,6 +32,10 @@ def product(n, term):
     162
     """
     "*** YOUR CODE HERE ***"
+    total = 1
+    for i in range(1, n+1):
+        total *= term(i)
+    return total
 
 
 def accumulate(fuse, start, n, term):
@@ -54,8 +58,11 @@ def accumulate(fuse, start, n, term):
     19
     """
     "*** YOUR CODE HERE ***"
-
-
+    if n == 0:
+        return start
+    else:
+        return fuse(term(n), accumulate(fuse, start, n-1, term))
+    
 def summation_using_accumulate(n, term):
     """Returns the sum: term(1) + ... + term(n), using accumulate.
 
@@ -68,8 +75,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
-
+    return accumulate(add, 0, n, term)
 
 def product_using_accumulate(n, term):
     """Returns the product: term(1) * ... * term(n), using accumulate.
@@ -83,7 +89,7 @@ def product_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(mul, 1, n, term)
 
 
 def make_repeater(f, n):
@@ -100,4 +106,10 @@ def make_repeater(f, n):
     390625
     """
     "*** YOUR CODE HERE ***"
+    def repeat(x):
+        for i in range(n):
+            x = f(x)
+        return x    
+    return repeat
+
 
