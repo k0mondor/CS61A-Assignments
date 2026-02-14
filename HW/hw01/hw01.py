e5f7523f8003bae21b2ef6c73cc95efd4f4c1091
Cai Zichen <3234912331@qq.com>
2026-02-14 12:54:13 +0800

    完成 hw01 作业

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i*i + j*j + k*k - max(i, j, k)**2
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,7 +66,11 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    for i in range(n-1, 0, -1):
+        # 迭代从 n-1 到 0 步长为-1
+        if n % i == 0:
+            return i
+    
 
 def hailstone(n):
     """Print the hailstone sequence starting at n and return its
@@ -88,4 +92,16 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    count = 1
+    
+    while n != 1:
+        print(n)
+        count += 1
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+            
+    print(1)
+    return count
 
